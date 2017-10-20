09fcdf90e63de0a1ad7410b3e2475ecb0fef7aca
804912547@qq.com <804912547@qq.com>
2017-10-20 18:03:06 +0800

    切面表达式

---
+++ web/src/test/java/com/bbd/annotation/Audience.java
@@ -0,0 +1,18 @@
+package com.bbd.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Liuweibo
+ * @version Id: Audience.java, v0.1 2017/10/20 Liuweibo Exp $$
+ */
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Audience {
+
+}
+    
+    
